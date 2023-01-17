725049d6a2d84f059c1aa330cfba774d79006d9e
hinczhang <hinczhang@whu.edu.cn>
2023-01-17 21:02:23 +0100

    initial version of the frontend

--- FileManagerFront/src/components/AdminComponents/AdminUsers.vue
+++ FileManagerFront/src/components/AdminComponents/AdminUsers.vue
@@ -203,7 +203,12 @@ export default {
             })
         },
         handleDelete(row){
-            this.$axios({
+            this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                this.$axios({
                 method: 'post',
                 url: '/files',
                 data: {
@@ -239,6 +244,12 @@ export default {
                     type: 'error'
                 });
             })
+            }).catch(() => {
+                this.$message({
+                    type: 'info',
+                    message: '已取消删除'
+                });          
+            });
         },
         limitChange(row){
             this.$axios({
